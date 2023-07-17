67735938a0fe210f690958117a86cd1eceecca4a
andrres017 <andrres017@gmail.com>
2023-07-17 00:18:28 -0500

    crete users an methodo POST

---
+++ src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import {UsersService} from './users.service'
+import {CreateUserDto} from './dto/create-user.dto'
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private userService: UsersService) {}
+
+    @Post()
+    createUser(@Body() newUser: CreateUserDto){
+        return this.userService.createUser(newUser)
+    }
+}
